c6747b4ccaf25579ad24f7291615e821e4c34f7f
Sulav Shrestha <sthasulav8@gmail.com>
2026-03-22 06:30:34 -0500

    quizzz

--- app/quiz/page.jsx
+++ app/quiz/page.jsx
@@ -1,9 +1,6 @@
 'use client'
 import { useState, useEffect, use } from 'react'
 import { useRouter, useSearchParams } from 'next/navigation'
-
-
-import { useRouter } from 'next/navigation'
 // for the google calendar api
 import { scheduleReviewSession } from '@/lib/api'
 
